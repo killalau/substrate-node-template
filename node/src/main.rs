1f1b41c7ff18cd026c13c4a5e48bd5b6f212013f
Franky Lau <wfflau@polyu.edu.hk>
2021-03-22 12:33:33 +0800

    refactor(rename): rename clib to cfun

--- node/src/main.rs
+++ node/src/main.rs
@@ -8,14 +8,14 @@ mod cli;
 mod command;
 mod rpc;
 
-extern crate clib;
+extern crate cfun;
 fn main() -> sc_cli::Result<()> {
 	unsafe {
-		clib::say_hello();
+		cfun::say_hello();
 		let input1: i32 = 10;
 		let input2: u32 = 11;
-		println!("double_int({}) = {}", input1, clib::double_int(input1));
-		println!("double_uint({}) = {}", input2, clib::double_uint(input2));
+		println!("double_int({}) = {}", input1, cfun::double_int(input1));
+		println!("double_uint({}) = {}", input2, cfun::double_uint(input2));
 	}
 
 	command::run()
